1878af02eb37684b37d0b4c8ea87084b56f56384
sillymotives <pacman232@gmail.com>
2026-09-17 21:52:21 +0100

    Add cosmetic interaction achievements

---
+++ text-adventure-game/game/achievements.py
@@ -0,0 +1,7 @@
+ACHIEVEMENTS = {
+    "bartender_moisturized": "Bartender Moisturized",
+    "wall_licked": "Wall Licked",
+    "attempted_capitalism_on_stone": "Attempted Capitalism On Stone",
+    "applied_science": "Applied Science",
+    "training_arc": "Training Arc",
+}
